4e53f1a62c1b256f93e665cd2cc7340a1fb2dbe3
KANCHOEUN <kancho1216@naver.com>
2021-08-27 16:12:20 +0000

    ✨ Impl create-account logic with DTO

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,8 @@
-import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
@@ -9,5 +13,20 @@ export class UsersResolver {
   @Query((returns) => Boolean)
   hi() {
     return true;
+  }
+
+  @Mutation((returns) => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const output = await this.usersService.createAccount(createAccountInput);
+      return output;
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
   }
 }
