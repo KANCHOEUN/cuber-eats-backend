6357b4eb2ed7f312356104b7bddd5cd601c94d2f
KANCHOEUN <kancho1216@naver.com>
2021-09-01 10:53:56 +0000

    ✨ Impl custom providers related to JWT

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,11 +4,14 @@ import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account.dto';
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from 'src/jwt/jwt.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
+    private readonly jwtService: JwtService,
   ) {}
 
   async createAccount({
@@ -33,19 +36,20 @@ export class UsersService {
     email,
     password,
   }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
-    // make a JWT and give it to the user
     try {
       const user = await this.users.findOne({ email });
       if (!user) {
         return { ok: false, error: 'User not found' };
       }
       const passwordCorrect = await user.checkPassword(password);
-      if (!password) {
+      if (!passwordCorrect) {
         return { ok: false, error: 'Wrong password' };
       }
+
+      const token = this.jwtService.sign(user.id);
       return {
         ok: true,
-        token: 'lalalal',
+        token,
       };
     } catch (error) {
       return {
